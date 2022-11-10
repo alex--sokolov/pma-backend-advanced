9118d967b08129cb71ebe4151b70bbbb0238135e
Alexander Sokolov <thorpe.nulled@gmail.com>
2022-11-10 09:41:28 +0300

    refactor: add mongo_db connection

--- Project management application/src/index.ts
+++ Project management application/src/index.ts
@@ -1,12 +1,13 @@
 import mongoose from 'mongoose';
-import { PORT } from './constants';
+import { MONGODB_CLUSTER, MONGODB_PASS, MONGODB_USER, PORT } from './constants';
 
 import * as serverService from './services/server.service';
-
+const Password = MONGODB_PASS;
+const User = MONGODB_USER;
 
 (async () => {
   try {
-    await mongoose.connect('mongodb+srv://${UserName}:${Password}@${clasterInfo}.mongodb.net/managerApp');
+    await mongoose.connect(`mongodb+srv://${MONGODB_USER}:${MONGODB_PASS}@${MONGODB_CLUSTER}.mongodb.net/managerApp`);
     serverService.server.listen(process.env.PORT || PORT, function () {
       console.log('Сервер ожидает подключения...');
     })
