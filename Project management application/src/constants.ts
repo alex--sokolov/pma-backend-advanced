f34f8896cac1a7c0b240438d0003c08165296e72
Alexander Sokolov <thorpe.nulled@gmail.com>
2022-11-25 13:33:23 +0300

    feat: desciption to board, userName to token

--- Project management application/src/constants.ts
+++ Project management application/src/constants.ts
@@ -1,5 +1,5 @@
 export const PORT = 3000;
-export const SECRET_KEY = 'myVerySecretKey-04.05.2022';
-export const MONGODB_USER = 'USER';
-export const MONGODB_PASS = 'R1NHbBYj7f9wIpNO';
-export const MONGODB_CLUSTER = 'yzk1drg';
+export const SECRET_KEY = "myVerySecretKey-04.05.2022";
+export const MONGODB_USER = "reactFinal";
+export const MONGODB_PASS = "Y4yzrzsQpVMtKdbI";
+export const MONGODB_CLUSTER = "bhwouug";
--- Project management application/src/index.ts
+++ Project management application/src/index.ts
@@ -1,25 +1,23 @@
-import mongoose from 'mongoose';
-import { MONGODB_CLUSTER, MONGODB_PASS, MONGODB_USER, PORT } from './constants';
+import mongoose from "mongoose";
+import { MONGODB_CLUSTER, MONGODB_PASS, MONGODB_USER, PORT } from "./constants";
 
-import * as serverService from './services/server.service';
-const Password = MONGODB_PASS;
-const User = MONGODB_USER;
+import * as serverService from "./services/server.service";
 
 (async () => {
   try {
-    await mongoose.connect('mongodb+srv://reactFinal:R1NHbBYj7f9wIpNO@cluster0.yzk1drg.mongodb.net/?retryWrites=true&w=majority');
-    // await mongoose.connect(`mongodb+srv://${MONGODB_USER}:${MONGODB_PASS}@cluster0.${MONGODB_CLUSTER}.mongodb.net/?retryWrites=true&w=majority`);
+    // await mongoose.connect('mongodb+srv://reactFinal:R1NHbBYj7f9wIpNO@cluster0.yzk1drg.mongodb.net/?retryWrites=true&w=majority');
+    await mongoose.connect(
+      `mongodb+srv://${MONGODB_USER}:${MONGODB_PASS}@cluster0.${MONGODB_CLUSTER}.mongodb.net/?retryWrites=true&w=majority`
+    );
     serverService.server.listen(process.env.PORT || PORT, function () {
-      console.log('Сервер ожидает подключения...');
-    })
+      console.log("Сервер ожидает подключения...");
+    });
   } catch (error) {
     console.log(error);
   }
 })();
 
-
-
-process.on('SIGINT', async () => {
+process.on("SIGINT", async () => {
   await mongoose.disconnect();
   process.exit();
 });
