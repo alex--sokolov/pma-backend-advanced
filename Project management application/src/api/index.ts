01fc641eda9433b5f6810bf5aa07312bf7753e49
Alexander Sokolov <thorpe.nulled@gmail.com>
2022-12-28 15:10:49 +0300

    fix: deploy to vercel#1

---
+++ Project management application/src/api/index.ts
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+import { MONGODB_CLUSTER, MONGODB_PASS, MONGODB_USER, PORT } from "../constants";
+
+import * as serverService from "../services/server.service";
+
+(async () => {
+    try {
+        await mongoose.connect(
+            `mongodb+srv://${MONGODB_USER}:${MONGODB_PASS}@cluster0.${MONGODB_CLUSTER}.mongodb.net/?retryWrites=true&w=majority`
+        );
+        serverService.server.listen(process.env.PORT || PORT, function () {
+            console.log("Сервер ожидает подключения...");
+        });
+    } catch (error) {
+        console.log(error);
+    }
+})();
+
+process.on("SIGINT", async () => {
+    await mongoose.disconnect();
+    process.exit();
+});
